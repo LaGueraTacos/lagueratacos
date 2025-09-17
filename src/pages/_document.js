8c4cebcb64dba1553ca15a67c0e19e04caf1a322
Jonathan Gonzalez <jonathangonzalez537@icloud.com>
2025-09-16 20:34:30 -0700

    Website Icon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,6 +7,12 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Sirivennela&display=swap" rel="stylesheet" />
+        
+        {/* Favicon */}
+        <link rel="icon" href="/Logos/LaGueraLogo(Black).jpg" />
+        <link rel="shortcut icon" href="/Logos/LaGueraLogo(Black).jpg" />
+        <link rel="apple-touch-icon" href="/Logos/LaGueraLogo(Black).jpg" />
+        <meta name="msapplication-TileImage" content="/Logos/LaGueraLogo(Black).jpg" />
       </Head>
       <body>
         <Main />
